bdd82e95573748748832a8b6e20db2485321d321
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-05-26 00:38:22 -0300

    Ajuste index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div class="center-align">
 	<ul>
 		<div class="left-align">
-			<li><a href="read.php"><strong>Ver Catálogo</strong></a></li>
+			<li><a href="read.php"><strong>Visualizar Catálogo</strong></a></li>
 			<li><a href="insert.php"><strong>Adicionar um produto</strong></a></li>
 			<li><a href="update.php"><strong>Atualizar um produto</strong></a></li>
 			<li><a href="delete.php"><strong>Remover um produto</strong></a></li>
