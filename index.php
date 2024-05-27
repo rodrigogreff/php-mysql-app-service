a0876675561081b96f9d67c4c60abafdec1a57ea
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-05-27 11:29:17 -0300

    Index tradução

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <div class="center-align">
 	<ul>
 		<div class="left-align">
-			<li><a href="read.php"><strong>Visualizar Catálogo</strong></a></li>
-			<li><a href="insert.php"><strong>Adicionar um produto</strong></a></li>
-			<li><a href="update.php"><strong>Atualizar um produto</strong></a></li>
-			<li><a href="delete.php"><strong>Remover um produto</strong></a></li>
+			<li><a href="read.php"><strong>Visualizar Catálogo1</strong></a></li>
+			<li><a href="insert.php"><strong>Adicionar um produto1</strong></a></li>
+			<li><a href="update.php"><strong>Atualizar um produto1</strong></a></li>
+			<li><a href="delete.php"><strong>Remover um produto1</strong></a></li>
 		</div>
 	</ul>
 </div>
