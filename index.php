940f0c07c802ed0499e15bb7643ffe70a5b4cf33
Rodrigo Greff <67062452+rodrigogreff@users.noreply.github.com>
2024-05-27 13:55:58 -0300

    index default

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <div class="center-align">
 	<ul>
 		<div class="left-align">
-			<li><a href="read.php"><strong>Visualizar Catálogo1</strong></a></li>
-			<li><a href="insert.php"><strong>Adicionar um produto1</strong></a></li>
-			<li><a href="update.php"><strong>Atualizar um produto1</strong></a></li>
-			<li><a href="delete.php"><strong>Remover um produto1</strong></a></li>
+		<li><a href="read.php"><strong>View Catalog</strong></a></li>
+			<li><a href="insert.php"><strong>Add a Product</strong></a></li>
+			<li><a href="update.php"><strong>Update a Product</strong></a></li>
+			<li><a href="delete.php"><strong>Remove a Product</strong></a></li>
 		</div>
 	</ul>
 </div>
